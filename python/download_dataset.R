a1db48e6b3baf2fa550be63f5148d63b96e90f6d
Santiago Palmero Muñoz <santiago.palmero@piperlab.es>
2023-11-30 11:35:01 +0100

    backup before mlflow

--- python/download_dataset.R
+++ python/download_dataset.R
@@ -1,9 +1,15 @@
+# Execute with Rscript download_dataset.R
+
 # install.packages("tsibble")
 # install.packages("tsibbledata")
 
 library(tsibble)
 library(tsibbledata)
-aus_production
 
-write.table(aus_production, file = "../data-raw/aus_production.csv", sep = "|", row.names = FALSE)
-print("aus_production.csv file created!")
+# aus_production
+# write.table(aus_production, file = "../data-raw/aus_production.csv", sep = "|", row.names = FALSE)
+# print("aus_production.csv file created!")
+
+sunspots <- sunspot.year %>% as_tsibble()
+write.table(sunspots, file = "../data-raw/sunspots.csv", sep = "|", row.names = FALSE)
+print("sunspots.csv file created!")
